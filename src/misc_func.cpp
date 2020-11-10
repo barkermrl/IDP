34c73a452edd474444b5950dc0270969de6f5885
m.hinedi <mh2004@cam.ac.uk>
2020-11-10 18:01:19 +0000

    moved all the line following algorithms into misc_functions. This is to keep main.cpp clean and minimal. Also, all functions are defined in variables.h and are only accessible through functions in misc_func. Can change this later if required.

--- src/misc_func.cpp
+++ src/misc_func.cpp
@@ -1,12 +1,16 @@
-#include "misc_func.h"
 #include <Arduino.h>
+#include "variables.h"
+#include "misc_func.h"
 
+//Function to update the lists avg1,avg2,avg3 by deleting the oldest value on one end and inserting the newest value on the other
 void shft(double *svg, int len, double IN){ //*svg passes first argument of array into the function, len is the arrays length, IN is 1/0
     for(int j = 0; j < len-1; j++ ){
         svg[j] = svg[j+1];
     }
     svg[-1] = IN;
 }
+
+//Function to find the newest sensor values and update avg1,avg2,avg3 all at once 
 void update(double *svg1, double *svg2, double *svg3, int len){
 
     if (digitalRead(ls1) == HIGH) {
@@ -27,4 +31,104 @@ void update(double *svg1, double *svg2, double *svg3, int len){
     else{
       shft(&svg2[0], 10, 0);
     }
+}
+
+//Function to do a 180* turn (Assuming midde sensor comes of the line during the turn)
+void turn180(){
+
+  if(digitalRead(ls2) == LOW){
+     M1 -> run(FORWARD);
+     M2 -> run(BACKWARD);
+     M1 -> setSpeed(150);
+     M2 -> setSpeed(150);
+     while(digitalRead(ls2) == LOW){
+       delay(100);
+     }
+  }
+  bool x = true;
+  while(x == true){
+    if(digitalRead(ls2)==LOW){
+      M1 -> run(FORWARD);
+      M2 -> run(FORWARD);
+      M1 -> setSpeed(150);
+      M2 -> setSpeed(150);
+      x = false;
+    }
+  }
+}
+
+void line_follower_singleswitch(){
+      if (digitalRead(ls2) == LOW && change == false){
+        delay(50);
+        if (digitalRead(ls2) == LOW){ //to make sure the "low" reading isnt a sudden random drop
+            M2 -> setSpeed(150 + kw*dir);
+            M1 -> setSpeed(150 - kw*dir);
+            change = true;
+        }
+    }
+    if(digitalRead(ls2) == HIGH && change == true){
+        delay(50);
+        if (digitalRead(ls2) == HIGH){ //to make sure the "high" reading isnt a sudden random spike
+            dir = -1*dir;
+            M2 -> setSpeed(150 + kw*dir);
+            M1 -> setSpeed(150 - kw*dir);
+            change = false;
+            delay(50);
+        }
+    }
+}
+
+void setup_auto(int i){
+
+  Serial.begin(9600); // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+  AFMS.begin(); // create with the default frequency 1.6KHz
+
+  pinMode(ls1, INPUT); //left
+  pinMode(ls2, INPUT); //centre
+  pinMode(ls3, INPUT); //right
+
+  M1 -> run(FORWARD);
+  M2 -> run(FORWARD);
+
+  //Which line following algorithm to set up for
+  switch (i)
+  {
+  case 1: //Single swtich
+    M2 -> setSpeed(150 + kw*dir);
+    M1 -> setSpeed(150 - kw*dir);
+    break;
+  
+  case 2:// "running average method"
+    M2 -> setSpeed(150);
+    M1 -> setSpeed(150);
+    break;
+  }
+}
+
+void lineFollower_3switch(){
+
+    //read sensor values and add their values into the lists
+    update(&avg1[0], &avg2[0], &avg3[0], 10); 
+
+    // loop to find averages of the lists
+    double av[3] = {0,0,0};
+    for(int i = 0; i < 10; i++){
+        av[0] += avg1[i];
+        av[1] += avg2[i];
+        av[2] += avg3[i];
+    }
+    av[0] = av[0]/10;
+    av[1] = av[1]/10;
+    av[2] = av[2]/10;
+
+    //control algorithm
+    double e = av[0] + av[1] - av[2]; // e = av1 + av2 - ev3
+    double K = kp*e;
+
+    //change motor powers
+    M2 -> setSpeed(150 - K);
+    M1 -> setSpeed(150 + K);
+    delay(100);
+    
 }
--- src/misc_func.h
+++ src/misc_func.h
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#define ls1 6
-#define ls2 7 //Middle sensor
-#define ls3 8
-#define kp 50 //proportional coeff for control using running averages
-#define kw 10 //oscilatory coeff for control using single sensor. Higher kw causes higher oscilations
+#include <Wire.h>
 
 void shft(double *svg, int len, double IN);
 void update(double *svg1, double *svg2, double *svg3, int len);
+void line_follower_singleswitch();
+void lineFollower_3switch();
+void setup_auto(int i);
+void turn180();
