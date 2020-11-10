0cb85787313e1eb5410adfb0a337279341103839
m.hinedi <mh2004@cam.ac.uk>
2020-11-10 12:42:48 +0000

    added an alternative line following agorithm that uses a single optoswitch

---
+++ src/Line_follower_singleSwitch.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "misc_func.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *M1 = AFMS.getMotor(1); //Left
+// You can also make another motor on port M2
+//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *M2 = AFMS.getMotor(2); //Right
+
+int dir = 1; //1 corresponds to left, -1 to right
+bool change = true;
+
+
+void setup() {
+  Serial.begin(9600); // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  pinMode(ls1, INPUT); //left
+  pinMode(ls2, INPUT); //centre -- in this version of the code, we will only use the centre sensor
+  pinMode(ls3, INPUT); //right
+
+  M1 -> run(FORWARD);
+  M2 -> run(FORWARD);
+  M2 -> setSpeed(150 + kw*dir);
+  M1 -> setSpeed(150 - kw*dir);
+}
+
+void loop(){
+    //this algorithm tries to make the robot follow the line by oscillating along it. The advantage is that it only uses 1 sensor, leaving 3 for detecting junctions and targets
+    if (digitalRead(ls2) == LOW){
+        M2 -> setSpeed(150 + kw*dir);
+        M1 -> setSpeed(150 - kw*dir);
+        change = true;
+    }
+    else if(change == true){
+        dir = -1*dir;
+        M2 -> setSpeed(150 + kw*dir);
+        M1 -> setSpeed(150 - kw*dir);
+        change = false;
+    }
+    
+}
--- src/misc_func.h
+++ src/misc_func.h
@@ -2,7 +2,8 @@
 #define ls1 6
 #define ls2 7 //Middle sensor
 #define ls3 8
-#define kp 50
+#define kp 50 //proportional coeff for control using running averages
+#define kw 10 //oscilatory coeff for control using single sensor. Higher kw causes higher oscilations
 
 void shft(double *svg, int len, double IN);
 void update(double *svg1, double *svg2, double *svg3, int len);
