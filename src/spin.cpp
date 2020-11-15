edbc6e068abc76f9f6f98963afd0583a6fa92c72
m.hinedi <mh2004@cam.ac.uk>
2020-11-15 19:10:14 +0000

    updated spin function

--- src/spin.cpp
+++ src/spin.cpp
@@ -1,23 +1,30 @@
 #include <Arduino.h>
 #include "electronics.h"
 
-void spin180(int power)
+void spin180(int power, int direction) //direction tells you wether or not you should spin to the right (-1) or to the left (+1)
 {
     //Function to do a 180* turn (Assuming midde sensor comes of the line during the turn)
-    if (RMOnLine())
+    if (RMOnLine() or LMOnLine()) //occurs when either one of the sensors is on the line
     {
-        MR->run(FORWARD);
-        ML->run(BACKWARD);
+        if (direction == 1){
+            MR->run(FORWARD);
+            ML->run(BACKWARD);
+        }
+        else
+        {
+            MR->run(BACKWARD);
+            ML->run(FORWARD);
+        }
         MR->setSpeed(power);
         ML->setSpeed(power);
-        while (RMOnLine())
+        while (RMOnLine() or LMOnLine())
         {
             delay(100);
         }
     }
     for (int i = 0; i <= 10000, i++;)
     {
-        if (RMOnLine())
+        if (RMOnLine() && direction == 1)
         {
             MR->run(FORWARD);
             ML->run(FORWARD);
@@ -25,23 +32,13 @@ void spin180(int power)
             MR->setSpeed(0);
             break;
         }
+        if (LMOnLine() && direction == -1)
+        {
+            MR->run(FORWARD);
+            ML->run(FORWARD);
+            ML->setSpeed(0);
+            MR->setSpeed(0);
+            break;
     }
 }
 
-void spin90Left(int power)
-{
-    // Robot spins 90 left
-
-    /***********************
-    TODO
-    ***********************/
-}
-
-void spin90Right(int power)
-{
-    // Robot spins 90 right
-
-    /***********************
-    TODO
-    ***********************/
-}
